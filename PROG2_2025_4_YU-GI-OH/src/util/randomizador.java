bad3cb4ba1ce08fbe312d5d8d83e4e0aba146689
Sylky <sylky18@gmail.com>
2026-01-20 19:38:38 -0300

    feat: implementa sistema de geração aleatória de cartas
    - Criada a classe principal GeraCarta para instanciar cartas com nome, tipo, elemento, ataque e defesa.
    
    - Implementado o repositório tipos_elementos com listas pré-definidas de nomes e categorias.
    
    - Desenvolvidos utilitários de suporte: randomizador (para gerar números aleatórios) e sorteado (para recuperar itens de listas via índice).
    
    - Adicionada lógica de nomes compostos e definição de atributos de combate (0-3000).

---
+++ PROG2_2025_4_YU-GI-OH/src/util/randomizador.java
@@ -0,0 +1,11 @@
+package util;
+public class randomizador {
+
+    public static int randomizador (String range_max) {
+        int range_min = 0;
+        int range_max_int = Integer.parseInt(range_max);
+        int randomInt = (int)(Math.random() * (range_max_int - (range_min) + 1) + range_min);
+        return randomInt;
+    }
+
+}
---
+++ PROG2_2025_4_YU-GI-OH/src/util/sorteado.java
@@ -0,0 +1,10 @@
+package util;
+
+import java.util.List;
+
+public class sorteado {
+
+   public static String sorteado(List<String> lista_randomizador, int indice) {
+        return lista_randomizador.get(indice);
+    }
+}
